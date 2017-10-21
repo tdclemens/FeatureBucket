b747a5999485bb6da9319ad6fa22ff070dc66613
Tyler Clemens <tdclemens@gmail.com>
2017-10-21 11:19:00 -0700

    Add swagger and swagger ui using docker

--- features.rb
+++ features.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'bundler'
+require 'sinatra/swagger'
 Bundler.require(:default)
 require_relative 'models/user'
 
@@ -9,8 +10,20 @@ set :port, '3000'
 
 Mongoid.load! 'mongoid.yml'
 
+# Validate and initialize swagger config
+swagger('swagger/users.yml')
+
+# Write the swagger json to a shared volume
+File.open('/swagger/swagger_spec.json', 'w') do |f|
+  f.write(settings.swagger.spec.to_json)
+end
+
+configure do
+  enable :cross_origin
+end
+
 get '/' do
-  "Hello World #{params[:name]}".strip
+  redirect 'http://localhost'
 end
 
 get '/api/v1/users' do
